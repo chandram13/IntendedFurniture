35dd3310345c1a9890ee2b2cf9f9589c05d08af3
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-29 07:54:57 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+// Marvish Chandra
+#include <stdio.h>
+
+public static void interiorSeating(string[] sofa){
+	if sofa == "individual seating":
+	printf("A reccomendation is to purchase a recliner either being: high leg, rocking, swivel, or a power lift.")
+	if sofa == "multiple seating":
+	printf("A reccomendation is to purchase a recling, sleeper, or stationary sofa to hold multiple people.")
+	if sofa == "couple seating":
+	printf("A reccomendation is to purchase a loveseat that is either recling or stationary to hold two people.")
+	else: printf("Either give a valid input for seating type or your seating is for an exterior setting.")
+    return 0;
+}
+public static void exteriorSeating(string[] sofa){
+	if sofa == "individual seating":
+	printf("A reccomendation is to purchase an outdoor chair, adirondack seat, ottoman, or a raised lounge.")
+	if sofa == "multiple seating":
+    printf("A reccomendation is to purchase a loveseat sofa, sectional sofa, or seperate dining set.")
+	else: printf("Either give a valid input for seating type or your seating is for an interior setting.")
+    return 0;
+}
